3e13e35fcab776f2080ce3becec2eca5fffdfd3b
Patrick Kelley <PatrickO.kelley88@gmail.com>
2024-07-29 12:37:26 -0500

    Completed State Exercise

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
+
+
 function HomePage() {
+    //This will be used for input fields text value
+    const [movieTitle, setMovieTitle] = useState("");
+    //an Array that holds all the different movie titles
+    const[watchList, setWatchList] = useState([]);
+
     return(
         <>
             <h1>Movie Watchlist</h1>
-            <form>
+            <form onSubmit={(submitEvent) => {
+                submitEvent.preventDefault();
+                //updated our list of movies to include what the user typed into input field
+                setWatchList([...watchList, movieTitle]);
+                setMovieTitle("");
+            }}>
                 <label htmlFor="movieInput">Enter movie to watch later</label>
-                <input type="text" name="movieInput" id="movieInput"/>
+                <input 
+                    type="text" 
+                    name="movieInput" 
+                    id="movieInput"
+                    value={movieTitle}
+                    onChange={(changeEvent) => { 
+                        console.log(changeEvent);
+                        setMovieTitle(changeEvent.target.value);
+                    }}
+                /> 
                 <button>Add Movie</button>
             </form>
-            <ul>
+            <ul id="watchlist"> 
+                {watchList.map((title, index) => {
+                    return <li key={index}>{title}</li>
+                })}
                 <li>
                     <p>Jurassic Park</p>
                 </li>
